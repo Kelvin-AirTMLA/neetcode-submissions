60bacbabbce8285a6126f2464a530e8eb2968acf
Kelvin-Air Erayanmen <87705171+Kelvin-AirTMLA@users.noreply.github.com>
2026-07-07 14:25:26 +0200

    Update: is-anagram - submission-3

--- Data Structures & Algorithms/is-anagram/submission-3.py
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -1,18 +1,8 @@
+from collections import Counter
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        s_cnt = Counter(s)
+        t_cnt = Counter(t)
 
-        chars = set()
-        official_letter_count_map = dict()
-        
-        
-        for letter in s:
-            chars.add(letter)
-            official_letter_count_map[letter] = official_letter_count_map.get(letter, 0) + 1
-
-        for letter in t:
-            if letter not in chars or t.count(letter) < official_letter_count_map[letter] or t.count(letter) > official_letter_count_map[letter]:
-                return False
-
-        return True
+        return s_cnt == t_cnt
