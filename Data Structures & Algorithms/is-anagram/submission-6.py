a6705d4819a6c82774df8654f0ee6c84c7500a2b
Kelvin-Air Erayanmen <87705171+Kelvin-AirTMLA@users.noreply.github.com>
2026-07-07 19:22:27 +0200

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_cnt = Counter(s)
+        t_cnt = Counter(t)
+        
+
+        return s_cnt == t_cnt
